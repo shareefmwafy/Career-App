74c371f70c2ed80b170c8e4f329d9ab2946c95ea
shareef <sh.mwafy1@gmail.com>
2024-09-30 15:37:01 +0300

    Create React Native Project

---
+++ Frontend/Mobile/app/(tabs)/index.tsx
@@ -0,0 +1,34 @@
+
+import {Text, View, ScrollView,SafeAreaView} from 'react-native'
+import { useState } from 'react';
+import { Stack,useRouter } from 'expo-router';
+// import {Colors} from '../../constants';
+
+export default function HomeScreen() {
+
+  const router = useRouter();
+  return (
+    <SafeAreaView>
+      <Text>Home</Text>
+    </SafeAreaView>
+  );
+}
+
+// const styles = StyleSheet.create({
+//   titleContainer: {
+//     flexDirection: 'row',
+//     alignItems: 'center',
+//     gap: 8,
+//   },
+//   stepContainer: {
+//     gap: 8,
+//     marginBottom: 8,
+//   },
+//   reactLogo: {
+//     height: 178,
+//     width: 290,
+//     bottom: 0,
+//     left: 0,
+//     position: 'absolute',
+//   },
+// });
---
+++ Frontend/Mobile/components/General Components/TestPage.tsx
@@ -0,0 +1,14 @@
+import { useLocalSearchParams } from "expo-router";
+import {Text} from 'react-native';
+
+export default function TestPage(){
+    const {slug} = useLocalSearchParams();
+
+    return(
+        <Text>Blog post:{slug}</Text>
+    );
+
+}
+
+
+
