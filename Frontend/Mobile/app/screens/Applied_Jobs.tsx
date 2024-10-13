c3d551d66562b63fb2d9ea02999d7bc34b9e9f3f
ayhamdw <s12029113@stu.najah.edu>
2024-10-13 10:46:01 +0300

    Add Pages For The Profile Setting

---
+++ Frontend/Mobile/app/screens/Applied_Jobs.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Applied_Jobs = () => {
+  return (
+    <View>
+      <Text>Applied_Jobs</Text>
+    </View>
+  );
+};
+
+export default Applied_Jobs;
---
+++ Frontend/Mobile/app/screens/Change_Password.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const Change_Password = () => {
+  return (
+    <View>
+      <Text>Change_Password</Text>
+    </View>
+  );
+};
+
+export default Change_Password;
