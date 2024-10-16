821cff188def1fd77136d3c696b0bde0777a32a7
ayhamdw <s12029113@stu.najah.edu>
2024-10-16 10:15:15 +0300

    Remove Code Smells

--- Frontend/Mobile/app/types.ts
+++
@@ -1,9 +0,0 @@
-import { NativeStackNavigationProp } from "@react-navigation/native-stack";
-
-type RootStackParamList = {
-  Introduction_1: undefined; // No params expected for this screen
-  Introduction_2: undefined; // No params expected for this screen
-};
-
-// Define the navigation prop type based on the RootStackParamList
-export type NavigationProp = NativeStackNavigationProp<RootStackParamList>;
