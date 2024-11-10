7d9306911b8c62f0d995ae06f3b09e4910dd215f
ayhamdw <s12029113@stu.najah.edu>
2024-11-10 13:44:24 +0200

    Structure The Files

---
+++ Backend/src/controllers/messagesController.js
@@ -0,0 +1,72 @@
+const User = require("../models/user"); //! User Model Object
+const Message = require("../models/message"); //! Message Model Object
+const messageController = async (req, res) => {
+  try {
+    console.log("inside message controller");
+    console.log(req.body);
+    const { senderId, receiverId, messageType, messageText } = req.body;
+
+    if (messageType === "image" && !req.file) {
+      console.error("File upload failed: req.file is undefined");
+      return res.status(400).json({ error: "File upload failed" });
+    }
+
+    if (
+      !senderId ||
+      !receiverId ||
+      !messageType ||
+      (!messageText && messageType !== "image")
+    ) {
+      return res.status(400).json({ error: "Missing required fields" });
+    }
+
+    const message = new Message({
+      senderId,
+      receiverId,
+      messageType,
+      messageText: messageType === "text" ? messageText : null,
+      timestamp: new Date(),
+      messageUrl: messageType === "image" ? req.file.path : null,
+    });
+
+    await message.save();
+    console.log("Message saved successfully");
+    res.status(200).json({ message: "Message Sent Successfully" });
+  } catch (error) {
+    console.error("Error while saving message:", error); // Log error for debugging
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const getChatUserDetails = async (req, res) => {
+  // console.log("inside get chat user details");
+  try {
+    const userId = req.params.userId;
+    await User.findById(userId)
+      .then((user) => {
+        res.status(200).json(user);
+      })
+      .catch((error) => {
+        console.log("error while finding user", error);
+        res.status(500).json({ Error: error });
+      });
+  } catch (error) {
+    res.status(500).json({ Error: error });
+  }
+};
+
+const getMessageBetweenUsersController = async (req, res) => {
+  try {
+    const { senderId, receiverId } = req.params;
+    const message = await Message.find({
+      $or: [
+        { senderId: receiverId, receiverId: senderId },
+        { senderId: senderId, receiverId: receiverId },
+      ],
+    }).populate("senderId", "_id name");
+    res.status(200).json(message);
+  } catch (error) {
+    console.log("error while getting the messages", error);
+    res.status(500).json({ error: error });
+  }
+};
---
+++ Backend/src/controllers/passwordController.js
@@ -0,0 +1,77 @@
+const User = require("../models/user"); //! User Model Object
+const securePassword = require("../utils/securePassword");
+
+const oldPasswordChecker = async (req, res) => {
+  console.log("inside old password checker");
+
+  if (!req.user || !req.user.email) {
+    console.log("User not authenticated");
+    return res.status(401).send("User not authenticated");
+  }
+
+  console.log("User email:", req.user.email);
+  console.log("Old Password:", req.query.oldPassword);
+
+  try {
+    const user = await User.findByCredentials(
+      req.user.email,
+      req.query.oldPassword
+    );
+    console.log("User authenticated successfully");
+    res.send("true");
+  } catch (error) {
+    res.status(400).send("false");
+    console.log("Inside error:", error);
+  }
+};
+const forgotPasswordController = async (req, res) => {
+  const { username } = req.body;
+  try {
+    const user = await User.findOne({ username: username }).lean();
+    if (!user) {
+      res.status(404).send("User not found");
+    } else {
+      const email = user.email.toString();
+      const code = Math.floor(100000 + Math.random() * 900000);
+      await User.updateOne(
+        { username: username },
+        { resetCode: code, resetCodeExpires: Date.now() + 7 * 60 * 1000 }
+      );
+      sendCode(email, code);
+      res.status(200).send("Code sent successfully");
+    }
+  } catch (error) {
+    console.log("Error", error);
+    res.status(500).send("Internal Server Error");
+  }
+};
+
+const resetPasswordController = async (req, res) => {
+  const { username, code, password } = req.body.data;
+  try {
+    const user = await User.findOne({ username: username }).lean();
+    const correctCode = user.resetCode;
+    const expireDate = user.resetCodeExpires;
+    if (code.toString() === correctCode.toString()) {
+      if (new Date() > expireDate) {
+        console.log("Expire Date");
+        return res.status(403).send({ message: "The Code has been expired" });
+      } else {
+        const hashedPassword = securePassword(password);
+        await User.updateOne(
+          { username: username },
+          { password: (await hashedPassword).toString() }
+        );
+        console.log("Changed Successfully");
+        return res
+          .status(200)
+          .send({ message: "The Password Changed Successfully" });
+      }
+    } else {
+      console.log("Incorrect Code");
+      return res.status(403).send({ message: "Incorrect Code" });
+    }
+  } catch (error) {
+    console.log("Error", error);
+  }
+};
