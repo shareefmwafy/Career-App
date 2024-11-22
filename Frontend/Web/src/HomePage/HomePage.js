173d1ef2262c77a5be9e5eeb226d9ef4ea827f31
Shareef Mwafy <sh.mwafy1@gmail.com>
2024-11-22 19:48:35 +0200

    Edit Hero Section Images

--- Frontend/Web/src/HomePage/HomePage.js
+++ Frontend/Web/src/HomePage/HomePage.js
@@ -8,52 +8,52 @@ import {
   } from "react-icons/rx";
   
   import SpaceCity1 from "./1.jpg";
-  import SpaceCity2 from "./2.avif";
-  import SpaceCity3 from "./3.jpeg";
-  import SpaceCity4 from "./1.avif";
-  import SpaceCity5 from "./2.avif";
-  import SpaceCity6 from "./3.jpeg";
-  import SpaceCity7 from "./1.avif";
-  import SpaceCity8 from "./2.avif";
-  import SpaceCity9 from "./3.jpeg";
+  import SpaceCity2 from "./2.png";
+  import SpaceCity3 from "./3.jpg";
+  import SpaceCity4 from "./4.jpg";
+  import SpaceCity5 from "./5.jpg";
+  import SpaceCity6 from "./6.avif";
+  import SpaceCity7 from "./7.jpg";
+  import SpaceCity8 from "./8.avif";
+  import SpaceCity9 from "./9.jpg";
   
 
   
   export const ServiceData = [
     {
       icon: RxCrop,
-      title: "Development",
-      content: "Lorem ipsum dolor sit /amet, consectetur adipiscing elit.",
+      // title: "Development",
+      // content: "Lorem ipsum dolor sit /amet, consectetur adipiscing elit.",
       backgroundImage: SpaceCity9,
     },
     {
       icon: RxPencil2,
-      title: "Branding",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      // title: "Branding",
+      // content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
       backgroundImage: SpaceCity1,
     },
     {
       icon: RxDesktop,
-      title: "Design",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      // title: "Design",
+      // content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
       backgroundImage: SpaceCity6,
     },
     {
       icon: RxReader,
-      title: "Seo",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      // title: "Seo",
+      // content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
       backgroundImage: SpaceCity7,
     },
     {
       icon: RxAccessibility,
-      title: "Management",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      // title: "Management",
+      // content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
       backgroundImage: SpaceCity5,
     },
     {
       icon: RxRocket,
-      title: "Production",
-      content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+      // title: "Production",
+      // content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
       backgroundImage: SpaceCity8,
     },
   ];
