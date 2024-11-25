039d85a81464951061e0d0ca176ccf7a5cb244a5
Shareef Mwafy <sh.mwafy1@gmail.com>
2024-11-25 17:11:19 +0200

    Create Sginin Page Desgin

--- Frontend/Web/src/Components/Sginin/Signin.jsx
+++ Frontend/Web/src/Components/Sginin/Signin.jsx
@@ -1,8 +1,45 @@
 import React from 'react'
-
+import style from './Sginin.module.css'
+import {Link} from 'react-router-dom'
+import Logo from './logo.png'
 function Sginin() {
   return (
-    <div>Sginin</div>
+    <div className={style.container}>
+      <div className={style.sgininForm}>
+        <div className={style.inpustPart}>
+          <div className={style.headerInput}>
+            <p>Sign In</p>
+          </div>
+          <div className={style.inputFields}>
+            <div className={style.field}>
+              <label htmlFor="username">Username</label>
+              <input type="text" name='username' />
+            </div>
+            <div className={style.field}>
+              <label htmlFor="password">Password</label>
+              <input type="password" name='password' />
+            </div>
+          </div>
+          <button className={style.sgininButton}>Sgin in</button>
+          <div className={style.lastLine}>
+            <div className={style.remember}>
+              <input type="checkbox" name='remember'/>
+              <label htmlFor="remember">Remember Me</label>
+            </div>
+            <Link>Forgot Password</Link>
+          </div>
+        </div>
+
+        <div className={style.designPart}>
+          <h2>Welcome To Career</h2>
+          <img src={Logo} alt="Career Logo" />
+          <p>Don't have an account?</p>
+          <Link to='/login'>Sgin Up</Link>
+
+        </div>
+
+      </div>
+    </div>
   )
 }
 
