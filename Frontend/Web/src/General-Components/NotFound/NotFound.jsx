4c26452d85dc888b4051ff2299a797ae6b99b451
Shareef Mwafy <sh.mwafy1@gmail.com>
2024-12-07 23:41:43 +0200

    Error Design Edit

--- Frontend/Web/src/General-Components/NotFound/NotFound.jsx
+++ Frontend/Web/src/General-Components/NotFound/NotFound.jsx
@@ -1,14 +1,13 @@
 import React from 'react';
-import './NotFound.css'; 
-
+import styles from './NotFound.module.css'; 
 const NotFound = () => {
   return (
-    <div className='container'>
-      <div className="not-found-container">
-      <h1 className="not-found-title">404</h1>
-      <p className="not-found-text">Oops! The page you're looking for doesn't exist.</p>
-      <a href="/" className="back-home-link">Go Back Home</a>
-    </div>
+    <div className={styles.container}>
+      <div className={styles.notFoundContainer}>
+        <h1 className={styles.notFoundTitle}>404</h1>
+        <p className={styles.notFoundText}>Oops! The page you're looking for doesn't exist.</p>
+        <a href="/" className={styles.backHomeLink}>Go Back Home</a>
+      </div>
     </div>
   );
 };
