7f18dd34896960185b8b01204d421157916eab9e
Shareef Mwafy <sh.mwafy1@gmail.com>
2024-12-16 20:38:43 +0200

    Enabel Delete a post

--- Backend/src/controllers/postController.js
+++ Backend/src/controllers/postController.js
@@ -33,8 +33,29 @@ const getAllPosts = async (req, res) => {
   }
 };
 
+const deletePost = async (req, res) => {
+  try {
+    const postId = req.params.id; 
+    const userId = req.userId;
+    const post = await Post.findById(postId);
+    if (!post) {
+      return res.status(404).json({ message: 'Post not found.' });
+    }
+    // if (post.user !== userId) {
+    //   return res.status(403).json({ message: 'You are not authorized to delete this post.' });
+    // }
+    await Post.findByIdAndDelete(postId);
+    return res.status(200).json({ message: 'Post deleted successfully.' });
+  } catch (error) {
+    console.error('Error deleting post:', error);
+    return res.status(500).json({ message: 'Internal server error.' });
+  }
+};
+
+
 module.exports = {
   createPost,
   getAllPosts,
+  deletePost,
 };
 
--- Backend/src/routers/postRoutes.js
+++ Backend/src/routers/postRoutes.js
@@ -5,5 +5,9 @@ const auth = require("../middleware/auth");
 
 router.post("/post", auth, postController.createPost);
 router.get("/posts", postController.getAllPosts);
+router.delete("/deletePost/:id", (req, res, next) => {
+    console.log("Delete route hit with ID:", req.params.id);
+    next();
+}, postController.deletePost);
 
 module.exports = router;
