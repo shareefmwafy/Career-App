25c1e2f9642cf353038d43e8a43cba5ec4c6f669
Shareef Mwafy <sh.mwafy1@gmail.com>
2024-12-16 21:40:51 +0200

    Display Posted by on the post

--- Backend/src/controllers/postController.js
+++ Backend/src/controllers/postController.js
@@ -2,18 +2,21 @@ const Post = require("../models/posts");
 const User = require("../models/user2");
 
 const createPost = async (req, res) => {
-  const { title, content, careerCategory, location,numberOfWorker } = req.body;
+  const { title, content, careerCategory, location,numberOfWorker,userFirstName,userLastName } = req.body;
   const user = req.user; 
   const userRole = user.role; 
   try {
     const newPost = new Post({
       user: user._id, 
       userRole: userRole, 
+      userFirstName,
+      userLastName,
       title,
       content,
       careerCategory,
       location,
       numberOfWorker,
+      
     });
     await newPost.save();
     res.status(201).json(newPost);
